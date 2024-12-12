28fd7512949a5505c6f4187aaeb92eb03d3cafba
Gilang2812 <100253104+Gilang2812@users.noreply.github.com>
2024-12-12 21:46:01 +0700

    fetat : intersection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,15 @@ import { ServiceItem } from "../components/ServiceItem";
 import { Header } from "../components/Header";
 import { cards } from "../data/card";
 import { Card } from "../components/Card";
+import { useInView } from "react-intersection-observer";
 const Home = () => {
+    const {ref,inView} = useInView({triggerOnce:true,threshold:0.2})
+    const {ref:leftRef,inView:leftView} = useInView({triggerOnce:true,threshold:0.2 })
+    const {ref:rightRef,inView:rightView} = useInView({triggerOnce:true,threshold:0.2})
     return (
         <main className="container bg-[#F8FAFC] font-sans h-screen max-w-none overflow-x-hidden">
             <Header />
-            <section className="mb-8 animate-slideInLeft">
+            <section ref={leftRef} className={`mb-8 ${leftView?"animate-slideInLeft":"opacity-0"}`}>
                 <div className="flex     leading-loose           ">
                     {cards.map((card, index) => (
                         <Card
@@ -26,7 +30,7 @@ const Home = () => {
 
             <section className="relative  flex flex-col gap-8 ">
                 <div className="space-y-4 leading-loose w-full z-10 [&_h2]:text-xl [&_h2]:text-indigo-900 [&_h2]:font-semibold mx-auto px-12  -mb-8">
-                    <article className="space-y-2  animate-slideInRight">
+                    <article ref={rightRef} className={` space-y-2  ${rightView?"animate-slideInRight":"opacity-0"}`}>
                         <h2> Main service</h2>
                         <div className="shadow  bg-white flex font-normal    divide-x        text-xl">
                             <ServiceItem Icon={MdOutlineMosque} />
@@ -34,7 +38,7 @@ const Home = () => {
                             <ServiceItem Icon={MdOutlineHealthAndSafety} />
                         </div>
                     </article>
-                    <article className="space-y-2 animate-fade">
+                    <article ref={ref} className={`space-y-2  ${inView?'animate-fade':'opacity-0'}`}>
                         <h2>It&apos;s easy getting to the Central Station Complex!</h2>
                         <div className="bg-slate-200 overflow-hidden p-1 h-full rounded-xl">
                             <Map />
